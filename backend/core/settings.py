babb2bfe18142bcb53267f4aba1f73f66cc2e553
Yuon12 <yonaswendosen71@gmail.com>
2026-07-27 00:18:51 +0300

    fix: template backend import path in settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -112,17 +112,23 @@ MIDDLEWARE = [
 ROOT_URLCONF     = "core.urls"
 WSGI_APPLICATION = "core.wsgi.application"
 
-TEMPLATES = [{
-    "BACKEND": "django.template.backends.DjangoTemplates",
-    "DIRS": [BASE_DIR / "templates"],
-    "APP_DIRS": True,
-    "OPTIONS": {"context_processors": [
-        "django.template.context_processors.debug",
-        "django.template.context_processors.request",
-        "django.contrib.auth.context_processors.auth",
-        "django.contrib.messages.context_processors.messages",
-    ]},
-}]
+# CORRECT
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 DATABASES = {
     "default": env.db(
